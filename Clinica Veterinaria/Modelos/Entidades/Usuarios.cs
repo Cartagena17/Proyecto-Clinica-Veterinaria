3da7ddf14683b8c7c4e021cefd0554323d956269
Your Name <you@example.com>
2025-07-24 12:03:43 -0600

    cambios

---
+++ Clinica Veterinaria/Modelos/Entidades/Usuarios.cs
@@ -0,0 +1,40 @@
+﻿using Modelos.Conexion;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelos.Entidades
+{
+    public class Usuarios
+    {
+        private int usuarioId;
+        private string usuario;
+        private string contraseña;
+
+        public int UsuarioId { get => usuarioId; set => usuarioId = value; }
+        public string Usuario { get => usuario; set => usuario = value; }
+        public string Contraseña { get => contraseña; set => contraseña = value; }
+
+         
+
+        //public static DataTable CargarUsuarios()
+        //{
+        //    SqlConnection conexion = Conexiondb.conectar();
+
+        //    string comando = "select *from Usuarios;";
+
+        //    SqlDataAdapter ad = new SqlDataAdapter(comando, conexion);
+
+        //    DataTable dt = new DataTable();
+        //    ad.Fill(dt);
+
+        //    return dt;
+        //}
+
+
+    }
+}
--- Clinica Veterinaria/Vistas/Formularios/frmIncio-Sesion.cs
+++ Clinica Veterinaria/Vistas/Formularios/frmIncio-Sesion.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Modelos.Conexion;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -19,12 +21,34 @@ namespace Vistas.Formularios
 
         private void btnInicioSesion_Click(object sender, EventArgs e)
         {
-            
-            frmDashboardPrincipal dashboard = new frmDashboardPrincipal();
-             
-            dashboard.Show();
 
-           this.Close();
+            SqlConnection con = Conexiondb.conectar();
+            string usuario = txtUsuario.Text;
+            string contraseña = txtContraseña.Text;
+            string comando = "select *from usuario where nombre_Usuario = @usuario and clave = @clave;";
+            SqlCommand cmd = new SqlCommand(comando, con);
+            cmd.Parameters.AddWithValue("@usuario", usuario);
+            cmd.Parameters.AddWithValue("@clave", contraseña);
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.HasRows)
+            {
+                MessageBox.Show("Bienvenido");
+                frmDashboardPrincipal dashboard = new frmDashboardPrincipal();
+
+                dashboard.Show();
+
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Los Parametros no coinciden");
+            }
+
+           // frmDashboardPrincipal dashboard = new frmDashboardPrincipal();
+             
+           // dashboard.Show();
+
+           //this.Close();
             
         }
     }
